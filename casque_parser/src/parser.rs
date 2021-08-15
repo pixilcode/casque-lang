5c61accb7eabb3fdc98fb721c72e4c004812ccea
Brendon Bown <bhbochikens@gmail.com>
2021-08-14 23:12:20 -0400

    Made parsers for simple tokens

--- casque_parser/src/parser.rs
+++ casque_parser/src/parser.rs
@@ -1,1 +1,16 @@
+#[macro_use]
+macro_rules! parser_tests {
+	( $( $test_name:ident : $parser:ident { $( $input:expr => $output:expr );* } )* ) => {
+		$(
+			#[test]
+			fn $test_name() {
+				$(assert_eq!($parser($input), $output));*
+			}
+		)*
+	};
+}
+
+type Input<'a> = &'a str;
+type ScannerResult<'a, O> = nom::IResult<Input<'a>, O>;
+
 mod scanner;
